19625664e307c1f3f9cf93b376f8eebe02a8192c
Oscar Talero <talero@gmail.com>
2021-06-03 17:55:36 -0500

    funcion inscrito

--- funciones.py
+++ funciones.py
@@ -1,15 +1,35 @@
-class Queue:
-    def __init__(self):
-        self.items = []
- 
-    def vacia(self):
-        return self.items == []
- 
-    def entrar(self, item):
-        self.items.insert(0,item)
- 
-    def sacar(self):
-        return self.items.pop()
- 
-    def longitud(self):
-        return len(self.items)
+import datetime
+
+def validar(lista_postulados, estudiante):
+    for i in lista_postulados:
+        if i == estudiante:
+            print('Estudiante se encuentra en la lista')
+
+def inscribir(lista_postulados):
+    estudiantes = {}
+    for i in range(len(lista_postulados)):
+        print('Ingrese los datos del estudiante :')
+        codigo = lista_postulados[i]     
+        print('Codigo: ',codigo)
+        identificacion = int(input('Ingrese la identificacion'))
+        nombre = input('Ingrese el nombre')
+        edad = input('Ingresese la edad; ')
+        op = True
+        while not False:
+            print('Seleccione el programa al que desea inscibirse')
+            print('1. Ingenieria Civil')
+            print('2. Ingenieria de Sistemas')
+            print('3. Ingenierlia Electronica')
+            opcion = int(input())
+            if opcion == 1 :
+                programa = 'Ingenieria Civil'
+            if opcion == 2 :
+                programa = 'Ingenieria de Sistemas' 
+            if opcion == 3 :
+                programa = 'Ingenieria Electronica'
+            if op > 0 or op < 4 :
+                op = False
+            fecha = datetime.datetime.today()
+        estudiantes[codigo] = (identificacion,nombre,edad,programa,fecha)
+        print(estudiantes)
+    return estudiantes
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-lista = [11,22,33,44]
+from collections import deque
+import funciones as func
+
+cola = deque([11,22,33,44])
+lista_postulados = [11,22,33,44]
+estudiante = int(input('Ingresese estudiante: '))
+resultado = func.validar(lista_postulados,estudiante)
+func.inscribir(lista_postulados)
+

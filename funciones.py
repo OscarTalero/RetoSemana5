865bde04dffb5e091a465966b1c98e0a7dc9150e
Oscar Talero <talero@gmail.com>
2021-06-05 11:11:35 -0500

    valida einscibir ok

--- funciones.py
+++ funciones.py
@@ -1,35 +1,43 @@
 import datetime
 
-def validar(lista_postulados, estudiante):
-    for i in lista_postulados:
-        if i == estudiante:
-            print('Estudiante se encuentra en la lista')
+def validar(lista_postulados, estudiante,lista_est):
+    if estudiante in lista_postulados:
+        print('Estudiante ',estudiante,'se encuentra en la lista')
+        if lista_est:
+            if lista_est[0][0] == estudiante:
+                print("Ya inscrito")
+            else:
+                lista_est =inscribir(estudiante,lista_est)
+        else:
+            lista_est = inscribir(estudiante,lista_est)
+    else:
+        print('Estudiante',estudiante,' no se encuentra postulado')
+    print(lista_est)
+    return lista_est
 
-def inscribir(lista_postulados):
-    estudiantes = {}
-    for i in range(len(lista_postulados)):
-        print('Ingrese los datos del estudiante :')
-        codigo = lista_postulados[i]     
-        print('Codigo: ',codigo)
-        identificacion = int(input('Ingrese la identificacion'))
-        nombre = input('Ingrese el nombre')
-        edad = input('Ingresese la edad; ')
-        op = True
-        while not False:
-            print('Seleccione el programa al que desea inscibirse')
-            print('1. Ingenieria Civil')
-            print('2. Ingenieria de Sistemas')
-            print('3. Ingenierlia Electronica')
-            opcion = int(input())
-            if opcion == 1 :
-                programa = 'Ingenieria Civil'
-            if opcion == 2 :
-                programa = 'Ingenieria de Sistemas' 
-            if opcion == 3 :
-                programa = 'Ingenieria Electronica'
-            if op > 0 or op < 4 :
-                op = False
-            fecha = datetime.datetime.today()
-        estudiantes[codigo] = (identificacion,nombre,edad,programa,fecha)
-        print(estudiantes)
-    return estudiantes
+def inscribir(estudiante,lista_est):
+    print('Ingrese los datos del estudiante :')
+    codigo = estudiante   
+    print('Codigo: ',codigo)
+    identificacion = int(input('Ingrese la identificacion: '))
+    nombre = input('Ingrese el nombre: ')
+    edad = input('Ingresese la edad: ')
+    op = True
+    while op == True:
+        print('Seleccione el programa al que desea inscibirse')
+        print('1. Ingenieria Civil')
+        print('2. Ingenieria de Sistemas')
+        print('3. Ingenieria Electronica')
+        opcion = int(input(':'))
+        if opcion == 1 :
+            programa = 'Ingenieria Civil'
+        if opcion == 2 :
+            programa = 'Ingenieria de Sistemas' 
+        if opcion == 3 :
+            programa = 'Ingenieria Electronica'
+        if opcion > 0 or opcion < 4 :
+            op = False
+        fecha = datetime.datetime.today()
+    estudiantes = [codigo,identificacion,nombre,edad,programa,fecha]
+    lista_est.append(estudiantes)
+    return lista_est
--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from collections import deque
 import funciones as func
 
 cola = deque([11,22,33,44])
-lista_postulados = [11,22,33,44]
-estudiante = int(input('Ingresese estudiante: '))
-resultado = func.validar(lista_postulados,estudiante)
-func.inscribir(lista_postulados)
+lista_postulados = [11,22,33]
+lista_est = []
+op = 'S'
+while op == 'S':
+    estudiante = int(input('Ingrese estudiante: '))
+    resultado = func.validar(lista_postulados,estudiante,lista_est)
+    print(resultado)
+    op = input('Consultar otro estudiante? '+ 'S '+'N')
 
